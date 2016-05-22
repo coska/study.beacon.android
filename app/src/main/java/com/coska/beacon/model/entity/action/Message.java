0a0cec49b3724ca393f1a9d83eeb062f9e04c927
bada5421 <bada5421@gmail.com>
2016-05-22 11:46:25 -0400

    Update Message.java
    SMS function

--- app/src/main/java/com/coska/beacon/model/entity/action/Message.java
+++ app/src/main/java/com/coska/beacon/model/entity/action/Message.java
@@ -6,12 +6,23 @@ import org.json.JSONObject;
 
 public class Message extends Action {
 
+	public static final String DIAL_NUMBER = "dial_number";
+	public static final String SMS_MESSAGE = "sms_message";
+
 	protected Message(JSONObject json) {
 		super(json);
 	}
 
+	private String getDialNumber() {
+		return json.optString(DIAL_NUMBER);
+	}
+	private String getSmsMessage() {
+		return json.optString(SMS_MESSAGE);
+	}
+
 	@Override
 	public void perform(Context context) {
-		// TODO: send text message
+		SmsManager smsManager = SmsManager.getDefault();
+		smsManager.sendTextMessage(getDialNumber(), null, getSmsMessage(), null, null);
 	}
 }
