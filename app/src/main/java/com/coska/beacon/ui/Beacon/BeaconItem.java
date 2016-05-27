3705c9d539381f3fa70952b6ec01cbf372dd139e
Kyounghyun Ryu <eva700ca@gmail.com>
2016-05-27 01:21:07 -0400

    add beacon list - proto type

---
+++ app/src/main/java/com/coska/beacon/ui/Beacon/BeaconItem.java
@@ -0,0 +1,7 @@
+package com.coska.beacon.ui.Beacon;
+
+/**
+ * Created by eva70 on 2016-05-26.
+ */
+public class BeaconItem {
+}
---
+++ app/src/main/java/com/coska/beacon/ui/Beacon/BeaconItemAdapter.java
@@ -0,0 +1,12 @@
+package com.coska.beacon.ui.Beacon;
+
+/**
+ * Created by eva70 on 2016-05-26.
+ */
+public class BeaconItemAdapter {
+    public String beaconName;
+
+    public BeaconItemAdapter(String name){
+        this.beaconName = name;
+    }
+}
